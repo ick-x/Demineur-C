6e5e6e1d25ea139c2d1b57acddd7cb9cb81eb5f8
ickx <51644482+ickxs@users.noreply.github.com>
2021-11-22 21:37:28 +0100

    Code de show_position() & get_position()
    J'ai codé les fonctions show_position() et get_position()
    
    show_position() ne nous servira pas à la fin mais elle nous permet de visualiser toutes les coordonnées des cellules de la matrice et leur position réelle 
    get_position() nous permettra à l'inverse de récupérer uniquement les cordonnées des cellules en fonction de la position entrée (dans le 'in') 
    
    J'ai aussi ajouté quelques commentaires dans le code pour comprendre ce que j'ai décrit ici

--- code.cpp
+++ code.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <cassert>
  
- 
 using namespace std;
  
+
  
 void initialiser(int ***g, unsigned int& lignes, unsigned int& colonnes)
 {
+    // Initialise la grille
     *g = new int*[colonnes];
     for (unsigned int i = 0; i < colonnes; ++i)
     {
@@ -23,8 +24,8 @@ void initialiser(int ***g, unsigned int& lignes, unsigned int& colonnes)
  
  
 void afficher_grille(int **g, unsigned int& lignes, unsigned int& colonnes)
-{
- 
+{ 
+    // Affiche la grille
     for (unsigned int i = 1; i <= colonnes; ++i) 
     {
         cout << " ---";
@@ -48,6 +49,8 @@ void afficher_grille(int **g, unsigned int& lignes, unsigned int& colonnes)
 
 void show_position(int** g, unsigned int& lignes, unsigned int& colonnes) 
 {
+    // Cette fonction sert juste à vérifier si les cellues de la matrice correspondent bien à leur position réelle 
+    // Donc elle sert pas à grande chose en vrai
     int position(-1);
 
     cout << "cellule -> position : " << endl << endl;
@@ -62,24 +65,54 @@ void show_position(int** g, unsigned int& lignes, unsigned int& colonnes)
     }
 }
 
-void get_position(int **g, unsigned int& lignes, unsigned int& colonnes, unsigned int& position) 
+void get_position(int **g, unsigned int& lignes, unsigned int& colonnes, unsigned int& p) 
 {
+    unsigned int position(-1);
     
+    for (int l = 0; l < lignes; l++) 
+    {
+        for (int c = 0; c < colonnes; c++)
+        {
+            position += 1; 
+            if (position == p) 
+            {
+                // Ce 'if' servira à récupérer les coordonnées de la cellule correspondant à une position en entrée
+                // Il faudra donc remplacer le 'cout' par une autre instruction
+                cout << "la position " << p << " est dans la cellule " << "grille.[" << l << "][" << c << "]" << endl;
+            }   
+        }
+    }     
 }
  
 int main()
 {
-    unsigned int lignes, colonnes;
+    unsigned int lignes, colonnes, position;
  
     int** grille;
- 
-    cin >> lignes;
-    cin >> colonnes;
+    
+    cout << "lignes : ";    //
+    cin >> lignes;          // 
+                            // Ces quelques lignes serviront pour
+    cout << "colonnes : ";  // la programmation mais pour le projet
+    cin >> colonnes;        // final il faudra utiliser celle en 
+                            // commentaire en dessous (pour le in)
+    cout << "position : ";  //  
+    cin >> position;        //
+    
+    
+    //cin >> lignes >> colonnes >> position;
+
+    assert (position < (lignes * colonnes));
+
+    cout << endl;
  
     initialiser(&grille, lignes, colonnes);
     afficher_grille(grille, lignes, colonnes);
     cout << endl;
     show_position(grille, lignes, colonnes);
+    cout << endl << "get_position() : " << endl << endl;
+    get_position(grille, lignes, colonnes, position);
+    cout << endl;
  
     return 0;
 }
