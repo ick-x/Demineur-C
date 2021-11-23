c5e91a78754c7076604c3a86c5e79cba5218dff5
Azguegou <90449587+Azguegou@users.noreply.github.com>
2021-11-23 14:21:07 +0100

    orthographe et adaptation
    J'ai rien fait en vrai, j'ai juste corrigé des fautes et bidouiller un system pause pour qu'il se lance sur mon ordi

--- code.cpp
+++ code.cpp
@@ -1,125 +1,124 @@
 #include <iostream>
 #include <cassert>
- 
+
 using namespace std;
- 
+
 typedef char Item;
- 
+
 void initialiser(Item ***g, unsigned int& lignes, unsigned int& colonnes)
 {
-    // Initialise la grille
-    *g = new Item*[colonnes];
-    for (unsigned int i = 0; i < colonnes; ++i)
-    {
-        (*g)[i] = new Item[lignes];
-    }
-    for (unsigned int l = 0; l < lignes; l++)
-    {
-        for (unsigned int c = 0; c < colonnes; c++)
-        {
-            (*g)[l][c]='.';
-        }
-    }
+	// Initialise la grille
+	*g = new Item*[colonnes];
+	for (unsigned int i = 0; i < colonnes; ++i)
+	{
+		(*g)[i] = new Item[lignes];
+	}
+	for (unsigned int l = 0; l < lignes; l++)
+	{
+		for (unsigned int c = 0; c < colonnes; c++)
+		{
+			(*g)[l][c] = '.';
+		}
+	}
 }
- 
- 
+
+
 void afficher_grille(Item **g, unsigned int& lignes, unsigned int& colonnes)
-{ 
-    // Affiche la grille
-    for (unsigned int i = 1; i <= colonnes; ++i) 
-    {
-        cout << " ---";
-    }
-    cout << endl;
- 
-    for (unsigned int l = 0; l < lignes; l++) 
-    {
-        for (unsigned int c = 0; c < colonnes; c++)
-        {
-            cout << "| " << g[l][c] << " "; 
-        }
-        cout <<"|" << endl;
-        for (unsigned int i = 1; i <= colonnes; ++i) 
-        {
-            cout << " ---";
-        }
-        cout << endl;
-    }
-}
-
-void show_position(Item** g, unsigned int& lignes, unsigned int& colonnes) 
 {
-    // Cette fonction sert juste à vérifier si les cellues de la matrice 
-    // correspondent bien à leur position réelle. 
+	// Affiche la grille
+	for (unsigned int i = 1; i <= colonnes; ++i)
+	{
+		cout << " ---";
+	}
+	cout << endl;
 
-    int position(-1);
-    int p(0);
-
-    cout << "cellule -> position : " << endl << endl;
-
-    for (int l = 0; l < lignes; l++) 
-    {
-        for (int c = 0; c < colonnes; c++)
-        {
-            position += 1;
-            
-            cout << "grille[" << l << "][" << c << "]" << " -> " << position << endl;
-
-        }
-    }
-
+	for (unsigned int l = 0; l < lignes; l++)
+	{
+		for (unsigned int c = 0; c < colonnes; c++)
+		{
+			cout << "| " << g[l][c] << " ";
+		}
+		cout << "|" << endl;
+		for (unsigned int i = 1; i <= colonnes; ++i)
+		{
+			cout << " ---";
+		}
+		cout << endl;
+	}
 }
 
-void marquer_mine(Item **g, unsigned int& lignes, unsigned int& colonnes, unsigned int& mine) 
+void show_position(Item** g, unsigned int& lignes, unsigned int& colonnes)
 {
-    unsigned int position(-1);
-    char marquage_mine = 120;
-    
-    for (int l = 0; l < lignes; l++) 
-    {
-        for (int c = 0; c < colonnes; c++)
-        {
-            position += 1; 
-            if (position == mine)
-            {
-                g[l][c] = marquage_mine;   
-            }   
-        }
-    }     
+	// Cette fonction sert juste à vérifier si les cellules de la matrice 
+	// correspondent bien à leur position réelle. 
+
+	int position(-1);
+	int p(0);
+
+	cout << "cellule -> position : " << endl << endl;
+
+	for (int l = 0; l < lignes; l++)
+	{
+		for (int c = 0; c < colonnes; c++)
+		{
+			position += 1;
+
+			cout << "grille[" << l << "][" << c << "]" << " -> " << position << endl;
+		}
+	}
 }
 
- 
+void marquer_mine(Item **g, unsigned int& lignes, unsigned int& colonnes, unsigned int& mine)
+{
+	unsigned int position(-1);
+	char marquage_mine = 120;
+
+	for (int l = 0; l < lignes; l++)
+	{
+		for (int c = 0; c < colonnes; c++)
+		{
+			position += 1;
+			if (position == mine)
+			{
+				g[l][c] = marquage_mine;
+			}
+		}
+	}
+}
+
+
 int main()
 {
-    unsigned int lignes, colonnes, mine_marquee;
- 
-    Item** grille;
-    
-    cout << "Lignes : ";            //
-    cin >> lignes;                  // 
-                                    // Ces quelques lignes serviront pour
-    cout << "COlonnes : ";          // la programmation mais pour le projet
-    cin >> colonnes;                // final il faudra utiliser celle en 
-                                    // commentaire en dessous (pour le 'in')
-    cout << "Marquer une mine : ";  //  
-    cin >> mine_marquee;            //
-    
-    
-    //cin >> lignes >> colonnes >> position;
+	unsigned int lignes, colonnes, mine_marquee;
 
-    assert (mine_marquee < (lignes * colonnes));
+	Item** grille;
 
-    cout << endl;
- 
-    initialiser(&grille, lignes, colonnes);
-    afficher_grille(grille, lignes, colonnes);
+	cout << "Lignes : ";            //
+	cin >> lignes;                  // 
+									// Ces quelques lignes serviront pour
+	cout << "Colonnes : ";          // la programmation mais pour le projet
+	cin >> colonnes;                // final il faudra utiliser celle en 
+									// commentaire en dessous (pour le 'in')
+	cout << "Marquer une mine : ";  //  
+	cin >> mine_marquee;            //
 
-    cout << endl;
-    show_position(grille, lignes, colonnes);
-    cout << endl;
-    marquer_mine(grille, lignes, colonnes, mine_marquee);
-    cout << endl;
-    afficher_grille(grille, lignes, colonnes);
- 
-    return 0;
+
+	//cin >> lignes >> colonnes >> position;
+
+	assert(mine_marquee < (lignes * colonnes));
+
+	cout << endl;
+
+	initialiser(&grille, lignes, colonnes);
+	afficher_grille(grille, lignes, colonnes);
+
+	cout << endl;
+	show_position(grille, lignes, colonnes);
+	cout << endl;
+	marquer_mine(grille, lignes, colonnes, mine_marquee);
+	cout << endl;
+	afficher_grille(grille, lignes, colonnes);
+
+	system("pause");
+	return 0;
 }
